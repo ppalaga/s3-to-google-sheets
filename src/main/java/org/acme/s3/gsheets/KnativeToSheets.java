6d3ab89a5260f7eb661735008020d4d032b33a4d
Peter Palaga <ppalaga@redhat.com>
2022-05-10 00:03:47 +0200

    Parse JSON from Telegram

--- src/main/java/org/acme/s3/gsheets/KnativeToSheets.java
+++ src/main/java/org/acme/s3/gsheets/KnativeToSheets.java
@@ -1,4 +1,4 @@
-// camel-k: trait=knative-service.min-scale=0
+// camel-k: config=secret:google-secret trait=knative-service.min-scale=0
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -18,34 +18,42 @@
 package org.acme.s3.gsheets;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 
 import com.google.api.services.sheets.v4.model.ValueRange;
 
 /**
  * Camel route definitions.
  */
-public class S3ToSheets extends RouteBuilder {
+public class KnativeToSheets extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("aws2-s3:{{aws-s3.bucket-name}}")
+        from("knative:channel/feedback")
                 .log("${body}")
+                .unmarshal().json(JsonLibrary.Jackson, java.util.Map.class)
+                .log("unmarshalled: ${body}")
                 .process(exchange -> {
                     final Message m = exchange.getMessage();
+                    Map<String, Object> body = m.getBody(Map.class);
                     m.setHeader("CamelGoogleSheets.valueInputOption", "RAW");
                     m.setHeader("CamelGoogleSheets.values",
                             new ValueRange().setValues(
                                     Arrays.asList(
                                             Arrays.asList(
-                                                    m.getBody(String.class)))));
+                                                    body.get("dateTime"),
+                                                    body.get("userName"),
+                                                    body.get("text")
+                                                    ))));
 
                 })
                 // Throttle to avoid exceeding the default Google API limit of 60 requests per min
                 .throttle(1).timePeriodMillis(1500)
-                .to("google-sheets://data/append?spreadsheetId={{google-sheets.spreadsheet-id}}&range=Sheet1!A1:A1");
+                .to("google-sheets://data/append?spreadsheetId={{google-sheets.spreadsheet-id}}&range=Sheet1!A1:A3");
     }
 
 }
